fda3d504e86c775c312d8d1703861561225b4bc2
koda <kdkyung57@gmail.com>
2019-04-21 04:29:14 +0900

    feat: project creation

---
+++ src/main/java/me/ellie/demobootrestapi/RestDemoApplication.java
@@ -0,0 +1,13 @@
+package me.ellie.demobootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDemoApplication.class, args);
+    }
+
+}
