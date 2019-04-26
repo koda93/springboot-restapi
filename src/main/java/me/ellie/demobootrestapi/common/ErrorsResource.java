e27d29bb6132b658bcc962ed89a2f09be5433968
koda <kdkyung57@gmail.com>
2019-04-27 06:01:34 +0900

    feat: Implement GET /api

---
+++ src/main/java/me/ellie/demobootrestapi/common/ErrorsResource.java
@@ -0,0 +1,17 @@
+package me.ellie.demobootrestapi.common;
+
+import me.ellie.demobootrestapi.index.IndexController;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.validation.Errors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+public class ErrorsResource extends Resource<Errors> {
+
+    public ErrorsResource(Errors errors, Link... links) {
+        super(errors, links);
+        add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
+    }
+}
--- src/main/java/me/ellie/demobootrestapi/events/EventController.java
+++ src/main/java/me/ellie/demobootrestapi/events/EventController.java
@@ -1,5 +1,6 @@
 package me.ellie.demobootrestapi.events;
 
+import me.ellie.demobootrestapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -36,12 +37,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
         if(errors.hasErrors()){
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -56,5 +57,9 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 }
