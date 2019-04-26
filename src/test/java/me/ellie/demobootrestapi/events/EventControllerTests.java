324f99f6312a4e9cafd94c310ce2835f5dc0f8d3
koda <kdkyung57@gmail.com>
2019-04-27 02:32:22 +0900

    chore: Build Spring REST Docs

--- src/test/java/me/ellie/demobootrestapi/events/EventControllerTests.java
+++ src/test/java/me/ellie/demobootrestapi/events/EventControllerTests.java
@@ -87,8 +87,9 @@ public class EventControllerTests {
                 .andDo(document("create-event",
                         links(
                                 linkWithRel("self").description("link to self"),
-                                linkWithRel("query-events").description("link to query-events"),
-                                linkWithRel("update-event").description("link to update-events")
+                                linkWithRel("query-events").description("link to query events"),
+                                linkWithRel("update-event").description("link to update events an existing event"),
+                                linkWithRel("profile").description("link to update-events")
                                 ),
                         requestHeaders(
                                 headerWithName(HttpHeaders.ACCEPT).description("accept header"),
@@ -127,7 +128,8 @@ public class EventControllerTests {
                                 fieldWithPath("eventStatus").description("event status"), // response에 추가된 것
                                 fieldWithPath("_links.self.href").description("event status"),
                                 fieldWithPath("_links.query-events.href").description("link to query event list"),
-                                fieldWithPath("_links.update-event.href").description("link to update existing event")
+                                fieldWithPath("_links.update-event.href").description("link to update existing event"),
+                                fieldWithPath("_links.profile.href").description("link to profile")
                         )
                 ))
         ;
